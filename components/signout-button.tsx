e89fe49992a13913d24c7599964a18a7b23a106a
indiflex <indiflex1@gmail.com>
2025-09-01 21:34:48 +0900

    next-auth auth.ts

--- components/signout-button.tsx
+++ components/signout-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signOut } from '@/lib/auth';
+import { logout } from '@/app/sign/sign.action';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import { Button } from './ui/button';
@@ -8,8 +8,6 @@ import { Button } from './ui/button';
 export default function SignOutButton() {
   const session = useSession();
   if (!session?.data?.user) redirect('/');
-
-  const logout = async () => await signOut({ redirectTo: '/login' });
 
   return (
     <form action={logout}>
--- lib/auth.ts
+++ lib/auth.ts
@@ -11,7 +11,58 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  providers: [Google, Github, Kakao, Naver, Credentials],
+  providers: [
+    Google,
+    Github,
+    Kakao,
+    Naver,
+    Credentials({
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email@bookmark.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'password...',
+        },
+      },
+      async authorize(credentials) {
+        console.log('credentials>>', credentials);
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, profile }) {
+      console.log('🚀 ~ profile:', profile);
+      console.log('🚀 ~ user:', user);
+      return true;
+    },
+    async jwt({ token, user }) {
+      // jwt 방식, GET /api/auth/callback/google에는 user없음!
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+      }
+      return session;
+    },
+  },
+
+  trustHost: true,
+  jwt: { maxAge: 30 * 60 },
+  pages: {
+    // signIn: '/sign',
+    error: '/sign/error',
+  },
   session: {
     strategy: 'jwt',
   },
