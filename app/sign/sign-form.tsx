43d2167845e884fab73125a47d869a3aee1a6e2c
indiflex <indiflex1@gmail.com>
2025-09-13 14:15:30 +0900

    validation & password reset

--- app/sign/sign-form.tsx
+++ app/sign/sign-form.tsx
@@ -2,12 +2,10 @@
 
 import LabelInput from '@/components/label-input';
 import { Button } from '@/components/ui/button';
-import type { ValidError } from '@/lib/validator';
 import { LoaderPinwheelIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useActionState, useReducer } from 'react';
-import z from 'zod';
-import { authorize } from './sign.action';
+import { authorize, regist } from './sign.action';
 
 export default function SignForm() {
   const [isSignin, toggleSign] = useReducer(pre => !pre, false);
@@ -23,25 +21,10 @@ export default function SignForm() {
 }
 
 function SignIn({ toggleSign }: { toggleSign: () => void }) {
-  const makeLogin = async (formData: FormData) => {
-    // const email = formData.get('email');
-    // const passwd = formData.get('passwd');
-
-    // const validator = z
-    //   .object({
-    //     email: z.email('잘못된 이메일 형식입니다!'),
-    //     passwd: z.string().min(6, 'More than 6 characters!'),
-    //   })
-    //   .safeParse(Object.fromEntries(formData.entries()));
-
-    // if (!validator.success) {
-    //   console.log('Error:', validator.error);
-    //   return alert(validator.error);
-    // }
-
-    await authorize(formData);
-  };
-
+  const [validError, makeLogin, isPending] = useActionState(
+    authorize,
+    undefined
+  );
   return (
     <>
       <form action={makeLogin} className='flex flex-col space-y-3'>
@@ -49,6 +32,7 @@ function SignIn({ toggleSign }: { toggleSign: () => void }) {
           label='email'
           type='email'
           name='email'
+          error={validError}
           defaultValue={'jeonseongho@naver.com'}
           placeholder='email@bookmark.com'
         />
@@ -57,6 +41,7 @@ function SignIn({ toggleSign }: { toggleSign: () => void }) {
           label='password'
           type='password'
           name='passwd'
+          error={validError}
           defaultValue={'121212'}
           placeholder='your password..'
           className='my-3x'
@@ -72,11 +57,16 @@ function SignIn({ toggleSign }: { toggleSign: () => void }) {
             Remember me
           </label>
 
-          <Link href='#'>Forgot Password?</Link>
+          <Link href='/forgotpasswd'>Forgot Password?</Link>
         </div>
 
-        <Button type='submit' variant={'primary'} className='w-full'>
-          Sign In
+        <Button
+          type='submit'
+          variant={'primary'}
+          className='w-full'
+          disabled={isPending}
+        >
+          {isPending ? 'Signing...' : 'Sign In'}
         </Button>
       </form>
       <div className='mt-5 flex gap-10'>
@@ -90,28 +80,7 @@ function SignIn({ toggleSign }: { toggleSign: () => void }) {
 }
 
 function SignUp({ toggleSign }: { toggleSign: () => void }) {
-  const [validError, makeRegist, isPending] = useActionState(
-    async (_preValidError: ValidError | undefined, formData: FormData) => {
-      const validator = z
-        .object({
-          email: z.email(),
-          passwd: z.string().min(6),
-          passwd2: z.string().min(6),
-          nickname: z.string().min(3),
-        })
-        .refine(
-          ({ passwd, passwd2 }) => passwd === passwd2,
-          'Passwords are not matched!'
-        )
-        .safeParse(Object.fromEntries(formData.entries()));
-
-      if (!validator.success) {
-        const err = z.treeifyError(validator.error).properties;
-        return err;
-      }
-    },
-    undefined
-  );
+  const [validError, makeRegist, isPending] = useActionState(regist, undefined);
 
   return (
     <>
@@ -120,6 +89,7 @@ function SignUp({ toggleSign }: { toggleSign: () => void }) {
           label='email'
           type='email'
           name='email'
+          focus={true}
           error={validError}
           placeholder='email@bookmark.com'
         />
--- app/sign/sign.action.ts
+++ app/sign/sign.action.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { signIn, signOut } from '@/lib/auth';
+import { validate, type ValidError } from '@/lib/validator';
+import z from 'zod';
 
 type Provider = 'google' | 'github' | 'naver' | 'kakao';
 
@@ -10,7 +12,18 @@ export const login = async (provider: Provider, callback?: string) => {
 
 export const loginNaver = async () => login('naver');
 
-export const authorize = async (formData: FormData) => {
+// credential login (email, passwd)
+export const authorize = async (
+  _preValidError: ValidError | undefined,
+  formData: FormData
+) => {
+  const zobj = z.object({
+    email: z.email(),
+    passwd: z.string().min(6, 'More than 6 characters!'),
+  });
+  const [err] = validate(zobj, formData);
+  if (err) return err;
+
   try {
     await signIn('credentials', formData);
   } catch (error) {
@@ -21,4 +34,24 @@ export const authorize = async (formData: FormData) => {
 
 export const logout = async () => {
   await signOut({ redirectTo: '/sign' }); // QQQ: '/'
+};
+
+export const regist = async (
+  _preValidError: ValidError | undefined,
+  formData: FormData
+) => {
+  const zobj = z
+    .object({
+      email: z.email(),
+      passwd: z.string().min(6),
+      passwd2: z.string().min(6),
+      nickname: z.string().min(3),
+    })
+    .refine(
+      ({ passwd, passwd2 }) => passwd === passwd2,
+      'Passwords are not matched!'
+    );
+
+  const [err] = validate(zobj, formData);
+  return err;
 };
